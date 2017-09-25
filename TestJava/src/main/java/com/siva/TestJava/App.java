0fa4b9676c70dcb7c6fa934c1e60a14662a999ff
sivaraya <siva.rayapureddy@gmail.com>
2017-09-25 16:33:00 +0530

    gterd

--- TestJava/src/main/java/com/siva/TestJava/App.java
+++ TestJava/src/main/java/com/siva/TestJava/App.java
@@ -9,6 +9,6 @@ public class App
 	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!..Siva.22ll." );
+        System.out.println( "Hello World!..Siva.22l." );
     }
 }
