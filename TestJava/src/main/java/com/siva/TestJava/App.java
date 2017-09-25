608e9660c10abb87c2a77143e8683fb687800a24
sivaraya <siva.rayapureddy@gmail.com>
2017-09-25 16:52:30 +0530

    www

--- TestJava/src/main/java/com/siva/TestJava/App.java
+++ TestJava/src/main/java/com/siva/TestJava/App.java
@@ -9,6 +9,6 @@ public class App
 	
     public static void main( String[] args )
     {
-        System.out.println( "Hello Wod!..Si.2l." );
+        System.out.println( "Hello Wod.Si.2l." );
     }
 }
