c88afd99f1f9de66a44eca89f9a7be24f433ab5b
sivaraya <siva.rayapureddy@gmail.com>
2017-09-25 17:50:46 +0530

    ff

--- TestJava/src/main/java/com/siva/TestJava/App.java
+++ TestJava/src/main/java/com/siva/TestJava/App.java
@@ -9,6 +9,6 @@ public class App
 	
     public static void main( String[] args )
     {
-        System.out.println( "Hehdllo Wod.2lss" );
+        System.out.println( "Hehdllo Wod.2ls" );
     }
 }
