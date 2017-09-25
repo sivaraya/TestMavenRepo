8c0283b83b72fcaf6ff1fe27882d5869a5bba8d5
sivaraya <siva.rayapureddy@gmail.com>
2017-09-25 16:33:43 +0530

    fds

--- TestJava/src/main/java/com/siva/TestJava/App.java
+++ TestJava/src/main/java/com/siva/TestJava/App.java
@@ -9,6 +9,6 @@ public class App
 	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!..Siva.22l." );
+        System.out.println( "Hello Word!..Siva.22l." );
     }
 }
