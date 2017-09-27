b5139a2ea31fc82af8a50afdd6c598626c42b143
sivaraya <siva.rayapureddy@gmail.com>
2017-09-27 12:49:28 +0530

    dsjflks

--- TestJava/src/main/java/com/siva/TestJava/App.java
+++ TestJava/src/main/java/com/siva/TestJava/App.java
@@ -9,6 +9,6 @@ public class App
 	
     public static void main( String[] args )
     {
-        System.out.println( "ddfajhkjhkfdddddfdsjhsdj" );
+        System.out.println( "ddfajfdsjhsdj" );
     }
 }
