ace1f6723df5b3ba744a8ad99c4885cff5b55e02
sivaraya <siva.rayapureddy@gmail.com>
2017-09-28 13:34:15 +0530

    ksjdflksjdlfjl

--- TestJava/src/main/java/com/siva/TestJava/App.java
+++ TestJava/src/main/java/com/siva/TestJava/App.java
@@ -10,7 +10,7 @@ public class App
 	
     public static void main( String[] args )
     {
-        System.out.println( "dkfjdkdsdj" );
+        System.out.println( "dkfjdsdj" );
         JOptionPane.showMessageDialog(null, "Eggs are not supposed to be green.");
     }
 }
