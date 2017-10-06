2f8bd21085f65f9812745e05448e608cb05efffe
sivaraya <siva.rayapureddy@gmail.com>
2017-10-06 12:59:40 +0530

    slkjf

--- TestJava/src/main/java/com/siva/TestJava/App.java
+++ TestJava/src/main/java/com/siva/TestJava/App.java
@@ -4,13 +4,21 @@ import javax.swing.JOptionPane;
 
 public class App 
 {
-	int returnInt(int i){
-		return i;
+	int returnInt(){
+		int[] in={1,2,3,4,5};
+    	
+    	for(int i:in){
+    		if(i>3){
+    			return i;
+    		}
+    	}
+    	return 0;
 	}
 	
     public static void main( String[] args )
     {
-        System.out.println( "ddsdj" );
-        JOptionPane.showMessageDialog(null, "Eggs are not supposed to be green.");
+    	App a=new App();
+    	System.out.println(a.returnInt());
+    	
     }
 }
